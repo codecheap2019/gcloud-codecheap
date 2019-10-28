6c9eb40efc2c04e61360c103fbd6ecaac8d52c70
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2019-10-28 12:14:58 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
-try {
-	
-	$pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=','root','');
-} catch (PDOException $e) {
-		die($e->getMessage());
-	}
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once 'vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Find your Account Sid and Auth Token at twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+$sid    = "AC234ef0c84d395c2bb0ed0a190e22a951";
+$token  = "910ec3473dfe4a82887c40bcce3853c5";
+$twilio = new Client($sid, $token);
+
+$message = $twilio->messages
+                  ->create("+919674259155", // to
+                           array(
+                               "body" => "You Have an New Order. Check it out",
+                               "from" => "+12242698082"
+                           )
+                  );
+
+print($message->sid);
