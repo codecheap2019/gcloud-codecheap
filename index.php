d98da57fd2c11d29502f1e11c9ca71900451ba6b
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2020-01-21 16:18:09 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
+<?php
+   $host        = "host = 127.0.0.1";
+   $port        = "port = 5432";
+   $dbname      = "dbname = testdb";
+   $credentials = "user = postgres password=pass123";
 
-echo "Hello World!";
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+?>
