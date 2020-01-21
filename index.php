0bc9d80b72e9c380e7f3a9cc2bd1fe8c3673f674
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2020-01-21 16:22:29 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-   $host        = "host = 127.0.0.1";
+   $host        = "host = ec2-174-129-33-84.compute-1.amazonaws.com";
    $port        = "port = 5432";
    $dbname      = "dbname = testdb";
    $credentials = "user = postgres password=pass123";
