fa80f8fe97a8cd050bbf15f0201ae179640efe48
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2020-01-21 23:48:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
    $host        = "host = ec2-174-129-33-84.compute-1.amazonaws.com";
    $port        = "port = 5432";
-   $dbname      = "dbname = testdb";
+   $dbname      = "dbname = d46te2h5urrp0e";
    $credentials = "user = jfxjsafmfdzhzu password=eb5eed2f015da016e68cca3d97136fd97ff53e8ca34d3aadc426a8689308fe13";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
