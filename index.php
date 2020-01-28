365072e6cc56bf62cd3e80ac4b8914e675b92054
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2020-01-28 10:13:58 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+
 <?php
-$db = getenv("DATABASE_URL");
-echo $db;
+
+$database = "BLUDB";        # Get these database details from
+$hostname = "dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net";  # the web console
+$user     = "hrp43445";   #
+$password = "j35+80c30v1wk931";  #
+$port     = 50000;          #
+$ssl_port = 50001;          #
+
+# Build the connection string
+#
+$driver  = "DRIVER={IBM DB2 ODBC DRIVER};";
+$dsn     = "DATABASE=$database; " .
+           "HOSTNAME=$hostname;" .
+           "PORT=$port; " .
+           "PROTOCOL=TCPIP; " .
+           "UID=$user;" .
+           "PWD=$password;";
+$ssl_dsn = "DATABASE=$database; " .
+           "HOSTNAME=$hostname;" .
+           "PORT=$ssl_port; " .
+           "PROTOCOL=TCPIP; " .
+           "UID=$user;" .
+           "PWD=$password;" .
+           "SECURITY=SSL;";
+$conn_string = $driver . $dsn;     # Non-SSL
+$conn_string = $driver . $ssl_dsn; # SSL
+
+# Connect
+#
+$conn = odbc_connect( $conn_string, "", "" );
+if( $conn )
+{
+    echo "Connection succeeded.";
+
+    # Disconnect
+    #
+    odbc_close( $conn );
+}
+else
+{
+    echo "Connection failed.";
+}
+?>
